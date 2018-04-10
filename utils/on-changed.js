2b1f53c3758d66165233c0472d049f6a3d732469
ngx-devtools <aelbore.sg@gmail.com>
2018-04-10 21:04:38 +0800

    add on changed

--- utils/on-changed.js
+++ utils/on-changed.js
@@ -4,10 +4,9 @@ const { watch } = require('./build-config');
 
 const { streamToPromise } = require('@ngx-devtools/common');
 
-const onClientFileChanged = path => {
+const onClientFileChanged = (path) => {
   const file = path.replace('.scss', '.ts').replace('.css', '.ts').replace('.html', '.ts');
-  return streamToPromise(build(file, watch.dest))
-    .then(() => Promise.resolve(path));
+  return (path && path.includes('src')) ? streamToPromise(build(file, watch.dest)) : Promise.resolve();
 };
 
 module.exports = onClientFileChanged;
