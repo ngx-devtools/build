a3ec687cc79a34d075d7c97a70d5d65ea0855787
ngx-devtools <aelbore.sg@gmail.com>
2018-07-16 22:58:55 +0800

    update build code

--- build.ts
+++ build.ts
@@ -2,9 +2,15 @@ import { buildCopyPackageFile, rollupBuild, createRollupConfig, clean } from '@n
 
 const PKG_NAME = 'build';
 
+const rollupConfig = { 
+  input: `src/${PKG_NAME}.ts`, 
+  file: `dist/${PKG_NAME}.js`, 
+  format: 'cjs',
+  external: [ 
+    '@ngx-devtools/common' 
+  ]
+};
+
 Promise.all([ clean('dist') ]).then(() => {
-  return Promise.all([ 
-    buildCopyPackageFile(PKG_NAME),  
-    rollupBuild(createRollupConfig({ input: `src/${PKG_NAME}.ts`, file: `dist/${PKG_NAME}.js`, format: 'cjs' }))
-  ])
+  return Promise.all([ buildCopyPackageFile(PKG_NAME), rollupBuild(createRollupConfig(rollupConfig)) ])
 });
