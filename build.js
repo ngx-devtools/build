e26a76481fbcc31e13fc94e57d982421d5e4ac2d
aelbore <aelbore.sg@gmailcom>
2018-10-06 15:07:01 +0800

    [refactor]: remove code in the build, update dependencies

--- build.js
+++ build.js
@@ -1,8 +1,4 @@
 const { ngxBuild } =  require('@ngx-devtools/common');
 const { getRollupConfig } = require('./rollup.config'); 
 
-(async function() {
-  const PKG_NAME = 'build';
-  await ngxBuild(PKG_NAME, getRollupConfig());
-})();
-
+ngxBuild('build', getRollupConfig());
